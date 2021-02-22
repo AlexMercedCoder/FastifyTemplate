577ae601c0ff0bc5a85f81e793ff692d18406734
Alex Merced <alex@alexmerced.dev>
2021-02-21 20:00:06 -0500

    update

--- server.js
+++ server.js
@@ -3,11 +3,12 @@
 //////////////////////////
 const app = require("./controllers/routes");
 const PORT = process.env.PORT ? process.env.PORT : 3000;
+const {log} = require("mercedlogger")
 
 app
   .then((fastify) => {
     fastify.listen(PORT, () => {
-      console.log(`Listening on port ${PORT}`);
+      log.green("Server Start", `Listening on port ${PORT}`);
     });
   })
   .catch((err) => console.log(err));
